be6aebae651d8af284ad49bb999d343ac8761e54
Ataru Nakano <atarunakano@gmail.com>
2020-02-05 09:41:09 +0900

    useStateを使う

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Counter from "./components/counter";
+import Counter from "./components/Counter"
+import CounterHook from "./components/CounterHook";
 
 const App = () => {
   return (
     <div className="App">
       <Counter />
+      <CounterHook />
     </div>
   );
 }
