fb74c52e9fe9d0d9b7a7a6ac9c3b0f4cac38ad6d
Ataru Nakano <atarunakano@gmail.com>
2020-02-05 09:46:17 +0900

    prevCount

--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -3,14 +3,20 @@ import React, { useState } from 'react'
 const CounterHook = () => {
   const [count, setCount] = useState(0)
 
-  const incrementCount = () => {
-    setCount(count + 1)
+  const incrementCount1 = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+  const incrementCount10 = () => {
+    for (let i = 0; i < 10; i++) {
+      setCount(prevCount => prevCount + 1)
+    }
   }
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={incrementCount}>カウント +</button>
+      <button onClick={incrementCount1}>カウント +1</button>
+      <button onClick={incrementCount10}>カウント +10</button>
     </div>
   )
 }
